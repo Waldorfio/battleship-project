5d19f27c457cebfcf91c651ad4593a36a229ebac
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-20 15:16:09 +1000

    Create createShip factory function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ import './style.css';
 console.clear();
 console.log('-----------');
 
-//// LOGIC 1: LANDING PAGE
+/// Create Factory Functions
+
+function createShip(length, sunk, statusArr) {
+    return {
+        length,
+        sunk,
+        statusArr,
+        initialise() {
+            for (i=0; i<length; i++) {statusArr.push(false)};
+            console.log(statusArr);
+        },
+        hit(num) {
+            statusArr[num] = 'hit!';
+            console.log(statusArr);
+        },
+        isSunk() {
+            for (i=0; i<length; i++) {
+                if (statusArr[i] === 'hit!') {
+                    if (i === length-1) {return true} // Return true if you have reached last array elem
+                    continue
+                } else if (statusArr[i] != 'hit!') { // Return false if hit
+                    return false
+                }
+            }
+        },
+    };
+}
+// let ship = createShip(3, false, []);
+
+    //// LOGIC 1: LANDING PAGE
 
 wipeLanding = () => {
     // Extract Player Name
@@ -13,14 +42,14 @@ wipeLanding = () => {
     const landingPage = document.getElementById('landing-cont');
     landingPage.innerHTML = '';
     console.log('playerName: '+playerName);
+
     buildBoard()
 }
-
 // Add START GAME button logic
 const startButton = document.getElementById('start-button');
 startButton.addEventListener('click', wipeLanding);
 
-//// LOGIC 2: BUILD BOARD
+    //// LOGIC 2: BUILD BOARD
 function buildBoard() {
     // Build Board Container
     const buildCont = document.createElement('div');
@@ -46,3 +75,7 @@ function buildBoard() {
 function clickedSquare(i) {
     console.log('index: '+i)
 }
+
+// Sub logic 2.1: Randomize AI grid
+
+    //// LOGIC 3: PLAY GAME
