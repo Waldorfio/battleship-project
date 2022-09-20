a875ffc6daa31431773388433f1e446353a04e51
Waldorfio <walid.hanifi707@gmail.com>
2022-09-20 19:29:28 +1000

    Refactor code with linter

--- src/index.js
+++ src/index.js
@@ -6,104 +6,99 @@ console.log('-----------');
 
 /// Create Factory Functions
 
-function createShip(length, sunk, statusArr) {
-    return {
-        length,
-        sunk,
-        statusArr,
-        initialise() {
-            for (i=0; i<length; i++) {statusArr.push(false)};
-            console.log(statusArr);
-        },
-        hit(num) {
-            statusArr[num] = 'hit!';
-            console.log(statusArr);
-        },
-        isSunk() {
-            for (i=0; i<length; i++) {
-                if (statusArr[i] === 'hit!') {
-                    if (i === length-1) {return true} // Return true if you have reached last array elem
-                    continue
-                } else if (statusArr[i] != 'hit!') { // Return false if hit
-                    return false
-                }
-            }
-        },
-    };
+function createShip (length, sunk, statusArr) {
+  return {
+    length,
+    sunk,
+    statusArr,
+    initialise () {
+      for (let i = 0; i < length; i++) { statusArr.push(false) };
+      console.log(statusArr);
+    },
+    hit (num) {
+      statusArr[num] = 'hit!';
+      console.log(statusArr);
+    },
+    isSunk () {
+      for (let i = 0; i < length; i++) {
+        if (statusArr[i] === 'hit!') {
+          if (i === length - 1) { return true } // Return true if you have reached last array elem
+          continue
+        } else if (statusArr[i] !== 'hit!') { // Return false if hit
+          return false
+        }
+      }
+    }
+  };
 }
 // let ship = createShip(3, false, []);
 
-
 // Create an empty 10x10 array
-let boardArr = [];
-for (i=0; i<10; i++) {
-    boardArr.push([])
-    for (j=0; j<10; j++) {
-        boardArr [i].push([]);
-    }
+const boardArr = [];
+for (let i = 0; i < 10; i++) {
+  boardArr.push([])
+  for (let j = 0; j < 10; j++) {
+    boardArr[i].push([]);
+  }
 }
 
-function gameBoard() {
-    return {
-        placeShip(ship, x, y) {
-            if (ship.length + y < 10) { // If the ship will not be out of bounds
-                for (i=0; i<ship.length; i++) {
-                    boardArr[x][y+i] = ship;
-                }
-            } else {console.log('ship will not fit')}
-
-        },
-        receiveAttack(idx) {
-        },
+function gameBoard () {
+  return {
+    placeShip (ship, x, y) {
+      if (ship.length + y < 10) { // If the ship will not be out of bounds
+        for (let i = 0; i < ship.length; i++) {
+          boardArr[x][y + i] = ship;
+        }
+      } else { console.log('ship will not fit') }
+    },
+    receiveAttack (idx) {
     }
+  }
 }
 
+/// / LOGIC 1: LANDING PAGE
 
+const wipeLanding = () => {
+  // Extract Player Name
+  const playerName = document.getElementById('player-name').value;
+  // Wipe Landing Page
+  const landingPage = document.getElementById('landing-cont');
+  landingPage.innerHTML = '';
+  console.log('playerName: ' + playerName);
 
-
-    //// LOGIC 1: LANDING PAGE
-
-wipeLanding = () => {
-    // Extract Player Name
-    const playerName = document.getElementById('player-name').value;
-    // Wipe Landing Page
-    const landingPage = document.getElementById('landing-cont');
-    landingPage.innerHTML = '';
-    console.log('playerName: '+playerName);
-
-    buildBoard()
+  buildBoard()
 }
 // Add START GAME button logic
 const startButton = document.getElementById('start-button');
 startButton.addEventListener('click', wipeLanding);
 
-    //// LOGIC 2: BUILD BOARD
-function buildBoard() {
-    // Build Board Container
-    const buildCont = document.createElement('div');
-    buildCont.id = 'build-cont'
-    document.getElementById('outer-cont').appendChild(buildCont);
-    // Greet Player personally
-    const promptBuild = document.createElement('div');
-    promptBuild.id = 'prompt-build';
-    promptBuild.innerHTML = 'Build your board TEST##';
-    buildCont.appendChild(promptBuild);
-    // Build the grid
-    const squareCont = document.createElement('div');
-    squareCont.id = 'square-cont';
-    buildCont.appendChild(squareCont);
-    for (i=0; i<100; i++) {
-        const square = document.createElement('div');
-        square.className = 'square';
-        square.addEventListener('click', clickedSquare.bind(this,i));
-        squareCont.appendChild(square);
-    }
+/// / LOGIC 2: BUILD BOARD
+function buildBoard () {
+  // Build Board Container
+  const buildCont = document.createElement('div');
+  buildCont.id = 'build-cont'
+  document.getElementById('outer-cont').appendChild(buildCont);
+  // Greet Player personally
+  const promptBuild = document.createElement('div');
+  promptBuild.id = 'prompt-build';
+  promptBuild.innerHTML = 'Build your board TEST##';
+  buildCont.appendChild(promptBuild);
+  // Build the grid
+  const squareCont = document.createElement('div');
+  squareCont.id = 'square-cont';
+  buildCont.appendChild(squareCont);
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.addEventListener('click', clickedSquare.bind(this, i));
+    squareCont.appendChild(square);
+  }
 }
 
-function clickedSquare(i) {
-    console.log('index: '+i)
+function clickedSquare (i) {
+  console.log('index: ' + i)
 }
 
 // Sub logic 2.1: Randomize AI grid
 
-    //// LOGIC 3: PLAY GAME
+/// / LOGIC 3: PLAY GAME
