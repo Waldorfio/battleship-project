462a672e172c808a5d5ccb51edf3a1e9a3e49b8c
Waldorfio <walid.hanifi707@gmail.com>
2022-09-21 21:48:11 +1000

    Update tests

--- src/index.test.js
+++ src/index.test.js
@@ -8,21 +8,21 @@ const factoryBoard = require('./factories/factoryBoard');
 
 
 describe('testing factoryBoard', () => {
-  // Initialising
-  beforeAll(() => {
-    board = factoryBoard([]);
-    cruiser = factoryShip(3, false, []);
-    board.initialise();
-    cruiser.initialise();
-  });
-  // Test 1: Ship placed in bounds
-  test('place ship IN bounds of gameboard...', () => {
-    expect(board.placeShip(cruiser, 1, 4)).toBeTruthy();
-  });
-  // Test 2: Ship placed out of bounds
-  test('place ship OUT bounds of gameboard...', () => {
-    expect(board.placeShip(cruiser, 8, 2)).toBeFalsy();
-  });
+  // // Initialising
+  // beforeAll(() => {
+  //   board = factoryBoard([]);
+  //   ship = factoryShip(3, false, [], 'cruiser');
+  //   board.initialise();
+  //   ship.initialise();
+  // });
+  // // Test 1: Ship placed in bounds
+  // test('place ship IN bounds of gameboard...', () => {
+  //   expect(board.placeShip(ship, 1, 4)).toBeTruthy();
+  // });
+  // // Test 2: Ship placed out of bounds
+  // test('place ship OUT bounds of gameboard...', () => {
+  //   expect(board.placeShip(ship, 8, 2)).toBeFalsy();
+  // });
 });
 
 describe('testing factoryShip', () => {
