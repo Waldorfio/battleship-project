de0337671581a8733654783f21642d285a7eee4f
Waldorfio <walid.hanifi707@gmail.com>
2022-09-21 17:08:09 +1000

    Add gameboard tests

--- src/index.test.js
+++ src/index.test.js
@@ -7,23 +7,32 @@ const factoryShip = require('./factories/factoryShip');
 const factoryBoard = require('./factories/factoryBoard');
 
 
-// describe('testing gameloop', () => {
-//   // Initialising
+describe('testing factoryBoard', () => {
+  // Initialising
+  beforeAll(() => {
+    board = factoryBoard([]);
+    cruiser = factoryShip(3, false, []);
+    board.initialise();
+    cruiser.initialise();
+  });
+  // Test 1: Ship placed in bounds
+  test('place ship IN bounds of gameboard...', () => {
+    expect(board.placeShip(cruiser, 1, 4)).toBeTruthy();
+  });
+  // Test 2: Ship placed out of bounds
+  test('place ship OUT bounds of gameboard...', () => {
+    expect(board.placeShip(cruiser, 8, 2)).toBeFalsy();
+  });
+});
 
-//   // Test 1: ship sinking
-//   test('testing 10x10 gameboard...', () => {
-//     expect(createBoard()).toBeDefined();
-//   });
-// });
-
-describe('factoryShip testing', () => {
+describe('testing factoryShip', () => {
   // Initialising
   beforeAll(() => {
     ship = factoryShip(3, false, []);
     ship.initialise(); // Filling up the empty array with false values
   });
   // Test 1: ship sinking
-  test('testing ship sinking...', () => {
+  test('ship sinking...', () => {
     expect(ship.isSunk()).toBeFalsy();
   });
 });
