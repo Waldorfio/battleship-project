08ccf637474bf61416b3934949ec2d914c0ec43c
Waldorfio <walid.hanifi707@gmail.com>
2022-09-21 15:32:46 +1000

    Refactor code

--- src/factories/factoryBoard.js
+++ src/factories/factoryBoard.js
@@ -1,6 +1,17 @@
 /* eslint-disable semi */
-function factoryBoard () {
+function factoryBoard (boardArr) {
   return {
+    boardArr,
+    initialise () {
+      const boardArr = [];
+      for (let i = 0; i < 10; i++) {
+        boardArr.push([])
+        for (let j = 0; j < 10; j++) {
+          boardArr[i].push([]);
+        }
+      }
+      return boardArr
+    },
     placeShip (ship, x, y) {
       if (ship.length + y < 10) { // If the ship will not be out of bounds
         for (let i = 0; i < ship.length; i++) {
