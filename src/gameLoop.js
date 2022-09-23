673d689c4e43eda1c39aef1240c394301a35f8e6
Waldorfio <walid.hanifi707@gmail.com>
2022-09-23 12:28:07 +1000

    Update gameloop to enable user interaction

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,16 +1,22 @@
 /* eslint-disable semi */
-function gameLoop () {
-    console.log('running gameLoop')
-// Extract elements
-  const squares = document.querySelectorAll(':bot-squares > .square'); // Select all box classes
+
+function gameLoop (p1, p2, xy) {
+  console.log('running gameLoop...');
+  // Extract elements
+  const squares = document.getElementById('bot-squares').childNodes; // Select all box classes
+  let idx = 0; // Create an index to keep track of square numbers
   squares.forEach(square => {
-    square.addEventListener('click', firedShot.bind(this, square));
+    square.addEventListener('click', firedShot.bind(this, square, p2, xy(idx)));
+    idx += 1;
   });
 }
 
-const firedShot (square) {
-    console.log('clicked!')
-  square.style['background-color'] = 'red';
+function firedShot (square, p2, xy) {
+  square.style['background-color'] = 'blue';
+  const x = xy[0];
+  const y = xy[1];
+  p2.gameBoard.receiveAttack(x, y);
+  console.log(p2.gameBoard.boardArr);
 }
 
 module.exports = gameLoop;
