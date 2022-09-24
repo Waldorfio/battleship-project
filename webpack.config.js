cc6f0a64b30efe484d25c27b31feeaf08e6e6a6d
Waldorfio <walid.hanifi707@gmail.com>
2022-09-24 14:48:30 +1000

    Rename index.js to buildDOM.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/buildDOM.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
