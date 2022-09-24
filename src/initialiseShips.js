2cfebc3f6cbfdccfa5472ade276d8818c5b4bbeb
Waldorfio <walid.hanifi707@gmail.com>
2022-09-24 19:10:40 +1000

    Add new 'hit' object input

--- src/initialiseShips.js
+++ src/initialiseShips.js
@@ -1,21 +1,21 @@
 /* eslint-disable semi */
 const factoryShip = require('./factories/factoryShip');
 
-const playerCarrier = factoryShip(5, false, 'carrier', false);
-const playerBattleship = factoryShip(4, false, 'battleship', false);
-const playerCruiser = factoryShip(3, false, 'cruiser', false);
-const playerDestroyerOne = factoryShip(2, false, 'first destroyer', false);
-const playerDestroyerTwo = factoryShip(2, false, 'second destroyer', false);
-const playerSubOne = factoryShip(1, false, 'first sub', false);
-const playerSubTwo = factoryShip(1, false, 'second sub', false);
+const playerCarrier = factoryShip(5, false, 'carrier', 0);
+const playerBattleship = factoryShip(4, false, 'battleship', 0);
+const playerCruiser = factoryShip(3, false, 'cruiser', 0);
+const playerDestroyerOne = factoryShip(2, false, 'first destroyer', 0);
+const playerDestroyerTwo = factoryShip(2, false, 'second destroyer', 0);
+const playerSubOne = factoryShip(1, false, 'first sub', 0);
+const playerSubTwo = factoryShip(1, false, 'second sub', 0);
 
-const botCarrier = factoryShip(5, false, 'carrier', false);
-const botBattleship = factoryShip(4, false, 'battleship', false);
+const botCarrier = factoryShip(5, false, 'carrier', 0);
+const botBattleship = factoryShip(4, false, 'battleship', 0);
 const botCruiser = factoryShip(3, false, 'cruiser', false);
-const botDestroyerOne = factoryShip(2, false, 'first destroyer', false);
-const botDestroyerTwo = factoryShip(2, false, 'second destroyer', false);
-const botSubOne = factoryShip(1, false, 'first sub', false);
-const botSubTwo = factoryShip(1, false, 'second sub', false);
+const botDestroyerOne = factoryShip(2, false, 'first destroyer', 0);
+const botDestroyerTwo = factoryShip(2, false, 'second destroyer', 0);
+const botSubOne = factoryShip(1, false, 'first sub', 0);
+const botSubTwo = factoryShip(1, false, 'second sub', 0);
 
 const playerShipArr = [playerCarrier, playerBattleship,
   playerCruiser, playerDestroyerOne, playerDestroyerTwo,
